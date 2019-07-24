cb1942c680b972e824272880856741049bc55e44
bellissima0419 <myrealgeorgia@gmail.com>
2019-07-24 19:32:43 -0400

    housekeeping

--- js/logic.js
+++ js/logic.js
@@ -1,21 +1,19 @@
-
-
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 const markerSize = mag => mag < 1 ? 1 : mag * 3
 
-const markerColor =  mag => {
-  return mag > 7 ? "#FF0000":
-  mag > 6 ? "#FF3400":
-  mag > 5 ? "#FF6900":
-  mag > 4 ? "#FF9E00":
-  mag > 3 ? "#FFC100":
-  mag > 2 ? "#FFE400":
-  mag > 1 ? "#F7FF00":
-              "#47FF00"
+const markerColor = mag => {
+  return mag > 7 ? "#FF0000" :
+    mag > 6 ? "#FF3400" :
+    mag > 5 ? "#FF6900" :
+    mag > 4 ? "#FF9E00" :
+    mag > 3 ? "#FFC100" :
+    mag > 2 ? "#FFE400" :
+    mag > 1 ? "#F7FF00" :
+    "#47FF00"
 }
 
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function (data) {
   console.log(data);
   createFeatures(data.features);
 });
@@ -25,28 +23,28 @@ function createFeatures(earthquakeData) {
 
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "<br>"
-      + `Magnitude: ${feature.properties.mag}`);
+      "</h3><hr><p>" + new Date(feature.properties.time) + "<br>" +
+      `Magnitude: ${feature.properties.mag}`);
   }
 
-  var earthquakes =  L.geoJSON(earthquakeData, {
+  var earthquakes = L.geoJSON(earthquakeData, {
     pointToLayer: ((feature, latlng) => {
-        return L.circleMarker(latlng, {
+      return L.circleMarker(latlng, {
 
-            radius: markerSize(feature.properties.mag),
-            fillOpacity: 0.5,
-            opacity: .8,
-            color: 'black',
-            fillColor: markerColor(feature.properties.mag),
-            weight: 0.4,
-        });
+        radius: markerSize(feature.properties.mag),
+        fillOpacity: 0.5,
+        opacity: .8,
+        color: 'black',
+        fillColor: markerColor(feature.properties.mag),
+        weight: 0.4,
+      });
 
     }),
     onEachFeature: onEachFeature
-})
+  })
 
   createMap(earthquakes);
-  
+
 }
 
 function createMap(earthquakes) {
@@ -76,7 +74,7 @@ function createMap(earthquakes) {
   };
 
   var myMap = L.map("map", {
-    center: [33.886917,9.537499],
+    center: [33.886917, 9.537499],
     zoom: 1.6,
     layers: [lightmap, earthquakes]
     // layers: [lightmap]
@@ -91,23 +89,24 @@ function createMap(earthquakes) {
     position: "bottomright"
   });
 
-    var info = L.control({position: 'bottomright'});
+  var info = L.control({
+    position: 'bottomright'
+  });
 
-    info.onAdd = function () {
+  info.onAdd = function () {
 
-      var div = L.DomUtil.create('div', 'legend')
-      var mags = [0, 1, 2, 3, 4, 5, 6, 7]
+    var div = L.DomUtil.create('div', 'legend')
+    var mags = [0, 1, 2, 3, 4, 5, 6, 7]
 
-      for (var i = 0; i < mags.length; i++) {
-        div.innerHTML +=
-          '<b style="background:' + markerColor(mags[i]) + '">&nbsp;&nbsp;&nbsp;&nbsp;</b> ' +
-            mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
-      }
-    
-      return div;
-    };
-    
-    info.addTo(myMap);  
-  
+    for (var i = 0; i < mags.length; i++) {
+      div.innerHTML +=
+        '<b style="background:' + markerColor(mags[i]) + '">&nbsp;&nbsp;&nbsp;&nbsp;</b> ' +
+        mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
+    }
+
+    return div;
   };
 
+  info.addTo(myMap);
+
+};
